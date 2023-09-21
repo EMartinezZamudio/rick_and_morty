20aa56c8567e9d727124bfd5f21d8ab45e03abe4
Emmanuel <manumtz20@gmail.com>
2023-09-21 00:11:40 -0600

    se cambio el boton

--- src/views/Landing/Landind.jsx
+++ src/views/Landing/Landind.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <h2 className={welcome}>Bievenidos a la</h2>
       <h1 className={title}>App de Rick and Morty</h1>
       <Link to="/home">
-        <button className={btn}>Ingresar</button>
+        <button className={btn}>Cerrar</button>
       </Link>
     </section>
   );
