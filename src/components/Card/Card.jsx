9ba24a806ad26306a7dfe9bdbfb9053a2e0401a4
Emmanuel <manumtz20@gmail.com>
2023-10-02 17:02:30 -0600

    se integro la parte de redux

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addFav, removeFav } from "../../redux/actions";
 import {
   card,
   cardDescription,
   cardTitle,
   linkImg,
   linkName,
+  btnClose,
+  btnFav,
 } from "./Card.module.css";
+import { useState } from "react";
 
-import { Link } from "react-router-dom";
+export default function Card(props) {
+  const { onClose, name, species, gender, image, id } = props;
 
-export default function Card({ onClose, name, species, gender, image, id }) {
+  const myFavorites = useSelector((state) => state.myFavorites);
+
+  const dispatch = useDispatch();
+
+  const [isFav, setIsFav] = useState(false);
+
+  const handleFavorite = () => {
+    if (isFav === true) {
+      setIsFav(false);
+      dispatch(removeFav(id));
+    } else {
+      setIsFav(true);
+      dispatch(addFav(props));
+    }
+  };
+
+  console.log(myFavorites);
+
   return (
     <article className={card}>
       <div className={cardTitle}>
-        <button onClick={() => onClose(id)}>X</button>
+        <button className={btnClose} onClick={() => onClose(id)}>
+          X
+        </button>
         <Link className={linkName} to={`/detail/${id}`}>
           <h2>{name}</h2>
         </Link>
@@ -24,6 +50,9 @@ export default function Card({ onClose, name, species, gender, image, id }) {
       <Link className={linkImg} to={`/detail/${id}`}>
         <img src={image} alt="" />
       </Link>
+      <button className={btnFav} onClick={handleFavorite}>
+        {isFav ? "💖" : "🤍"}
+      </button>
     </article>
   );
 }
