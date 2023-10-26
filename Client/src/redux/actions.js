2d51a0f3d332e31f5fc72c35eed317e783624932
Emmanuel <manumtz20@gmail.com>
2023-10-26 16:58:59 -0600

    se cambiaron reducer

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,16 +1,31 @@
+// modulos
+import axios from "axios";
+
+// variales
 import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./actionTypes";
 
+// constantes
+const END_POINT = "http://localhost:3001/rickandmorty/fav";
+
 export const addFav = (char) => {
-  return {
-    type: ADD_FAV,
-    payload: char,
+  return (dispatch) => {
+    axios.post(END_POINT, char).then(({ data }) => {
+      return dispatch({
+        type: ADD_FAV,
+        payload: data,
+      });
+    });
   };
 };
 
 export const removeFav = (id) => {
-  return {
-    type: REMOVE_FAV,
-    payload: id,
+  return (dispatch) => {
+    axios.delete(`${END_POINT}/${id}`).then(({ data }) => {
+      return dispatch({
+        type: REMOVE_FAV,
+        payload: data,
+      });
+    });
   };
 };
 
