8c18e8a4a9205d12ae9e89d51aaaf87d20c57f7d
Emmanuel <manumtz20@gmail.com>
2023-11-13 22:36:29 -0600

    integracion de base de datos

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,18 @@
 const server = require("./server");
+const { conn } = require("./DB_connection");
 
 // variables
 const PORT = 3001;
 
-// salida del servidor
-server.listen(PORT, () => {
-  console.log(`Server raised in port: ${PORT}`);
-});
+// conexion a base de datos
+conn
+  .sync({})
+  .then(() => {
+    // salida del servidor
+    server.listen(PORT, () => {
+      console.log(`Server raised in port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
