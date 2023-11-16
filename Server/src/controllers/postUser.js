a30ebcbcf873adb612ba2c5ebcd0eb8e3a469882
Emmanuel <manumtz20@gmail.com>
2023-11-16 12:17:04 -0600

    se corrigio logica

--- Server/src/controllers/postUser.js
+++ Server/src/controllers/postUser.js
@@ -1,18 +1,24 @@
 const { User } = require("../DB_connection");
 
 const postUser = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) res.status(400).json({ error: "Faltan datos" });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Faltan datos" });
+    }
 
-  const [user, created] = await User.findOrCreate({
-    where: { email },
-    defaults: { password },
-  });
+    const [user, created] = await User.findOrCreate({
+      where: { email },
+      defaults: { password },
+    });
 
-  if (created) {
-    res.status(200).json({ message: "Usuario Guardado" });
-  } else {
-    res.status(400).json({ error: "El usuario ya existe" });
+    if (created) {
+      res.status(200).json({ message: "Usuario Guardado" });
+    } else {
+      res.status(400).json({ message: "El usuario ya existe" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
