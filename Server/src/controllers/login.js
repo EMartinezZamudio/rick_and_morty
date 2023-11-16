df098fa09803c0d9f403c0005d2e0dcf9a7152db
Emmanuel <manumtz20@gmail.com>
2023-11-16 12:47:58 -0600

    nuevo controlador login

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,25 +1,39 @@
-const users = require("../utils/users");
+const { User } = require("../DB_connection");
 
-const login = (req, res) => {
-  const { email, password } = req.query;
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.query;
+    if (!email || !password) {
+      return res.status(400).json({ messege: "Faltan datos" });
+    }
 
-  // users.forEach((user) => {
-  //   if (user.email === email && user.password === password) {
-  //     res.status(200).json({ access: true });
-  //     return;
-  //   }
-  // });
+    const user = await User.findOne({ where: { email } });
 
-  const user = users.find((user) => {
-    return user.email === email && user.password === password;
-  });
-
-  user
-    ? res.status(200).json({ access: true })
-    : res.status(200).json({ access: false });
-
-  // res.status(200).json({ access: false });
-  // return;
+    if (user) {
+      password === user.password
+        ? res.status(200).json({ access: true })
+        : res.status(403).json({ message: "Contraseña incorrecta" });
+    } else {
+      res.status(404).json({ messege: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
+
+// const users = require("../utils/users");
+
+// const login = (req, res) => {
+//   const { email, password } = req.query;
+
+//   const user = users.find((user) => {
+//     return user.email === email && user.password === password;
+//   });
+
+//   user
+//     ? res.status(200).json({ access: true })
+//     : res.status(200).json({ access: false });
+
+// };
 
 module.exports = login;
